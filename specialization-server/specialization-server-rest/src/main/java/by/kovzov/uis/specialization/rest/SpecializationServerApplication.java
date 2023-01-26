1f25f1f9f450416150f647c27df0c63f9ebf561b
Uladzislau <fenix23707@gmail.com>
2023-01-26 21:47:55 +0300

    Remove security from specialization server

--- specialization-server/specialization-server-rest/src/main/java/by/kovzov/uis/specialization/rest/SpecializationServerApplication.java
+++ specialization-server/specialization-server-rest/src/main/java/by/kovzov/uis/specialization/rest/SpecializationServerApplication.java
@@ -2,15 +2,8 @@ package by.kovzov.uis.specialization.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import by.kovzov.uis.auth.rest.security.RsaKeyProperties;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-
-//TODO:remove
 @SpringBootApplication(scanBasePackages = {"by.kovzov.uis"})
-@OpenAPIDefinition
-@EnableConfigurationProperties(RsaKeyProperties.class) //TODO:remove
 public class SpecializationServerApplication {
 
     public static void main(String[] args) {
