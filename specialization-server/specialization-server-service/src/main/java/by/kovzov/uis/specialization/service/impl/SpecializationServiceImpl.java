de01d833cf4d48e51c5a0a563614578a73dc699d
Uladzislau <fenix23707@gmail.com>
2023-01-28 12:46:06 +0300

    Fix n+1 issue

--- specialization-server/specialization-server-service/src/main/java/by/kovzov/uis/specialization/service/impl/SpecializationServiceImpl.java
+++ specialization-server/specialization-server-service/src/main/java/by/kovzov/uis/specialization/service/impl/SpecializationServiceImpl.java
@@ -2,11 +2,16 @@ package by.kovzov.uis.specialization.service.impl;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.function.Function;
+
 import by.kovzov.uis.specialization.dto.SpecializationParentDto;
 import by.kovzov.uis.specialization.repository.api.SpecializationRepository;
+import by.kovzov.uis.specialization.repository.entity.Specialization;
 import by.kovzov.uis.specialization.service.api.SpecializationService;
 import by.kovzov.uis.specialization.service.mapper.SpecializationMapper;
 import lombok.AllArgsConstructor;
@@ -21,10 +26,15 @@ public class SpecializationServiceImpl implements SpecializationService {
     @Override
     @Transactional(readOnly = true)
     public Page<SpecializationParentDto> getAllParents(Pageable pageable) {
-        return specializationRepository.findAllParents(pageable)
-            .map(specializationMapper::toParentDto)
-            .map(dto -> dto.toBuilder()
-                .hasChildren(specializationRepository.countByParentId(dto.getId()) > 0)
-                .build());
+        Function<Specialization, SpecializationParentDto> mapToParentDto = entity -> {
+            return specializationMapper.toParentDto(entity).toBuilder()
+                .hasChildren(!entity.getChildren().isEmpty())
+                .build();
+        };
+
+        Page<Long> parentIds = specializationRepository.findAllParentIds(pageable);
+        List<Specialization> content = specializationRepository.findAllWithChildrenByIds(parentIds.toSet());
+        return PageableExecutionUtils.getPage(content, pageable, () -> parentIds.getTotalElements())
+            .map(mapToParentDto);
     }
 }
