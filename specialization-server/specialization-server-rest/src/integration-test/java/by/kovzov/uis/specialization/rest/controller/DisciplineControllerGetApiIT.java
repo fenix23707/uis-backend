90c13bb41cf306aa79a411bf0801ceda6d006551
Uladzislau <fenix23707@gmail.com>
2023-02-18 15:09:25 +0300

    Fix discipline integration tests

---
+++ specialization-server/specialization-server-rest/src/integration-test/java/by/kovzov/uis/specialization/rest/controller/DisciplineControllerGetApiIT.java
@@ -0,0 +1,111 @@
+package by.kovzov.uis.specialization.rest.controller;
+
+import static org.hamcrest.Matchers.blankOrNullString;
+import static org.hamcrest.Matchers.emptyOrNullString;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+import static java.text.MessageFormat.format;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import by.kovzov.uis.specialization.repository.api.DisciplineRepository;
+import by.kovzov.uis.specialization.repository.entity.Discipline;
+import by.kovzov.uis.specialization.rest.common.AbstractIntegrationTest;
+import io.restassured.http.ContentType;
+import lombok.Setter;
+
+@Setter
+class DisciplineControllerGetApiIT extends AbstractIntegrationTest {
+
+    private static final String BASE_URL = "/api/disciplines/";
+
+    @Autowired
+    private DisciplineRepository disciplineRepository;
+
+    private List<Discipline> disciplines;
+
+    @BeforeEach
+    void setUp() {
+        disciplines = dataLoader.loadJson(Discipline.class, "data/json/disciplines.json");
+        disciplineRepository.saveAll(disciplines);
+    }
+
+    @Test
+    void getByIdReturnValidResponse() {
+        int id = 1;
+
+        given()
+            .when()
+            .get(BASE_URL + id)
+            .then()
+            .statusCode(200)
+            .body("id", is(id))
+            .body("name", is("Spanish"))
+            .body("shortName", is("sp"));
+    }
+
+    @Test
+    void getByIdReturnNotFoundWhenIdNotExist() {
+        int id = 999999;
+
+        given()
+            .when()
+            .get(BASE_URL + id)
+            .then()
+            .statusCode(404)
+            .body("message", is(format("Discipline with id = {0} not found.", id)))
+            .body("path", is(BASE_URL + id));
+    }
+
+    @Test
+    void getByIdReturnValidJsonSchema() {
+        int id = 1;
+
+        given()
+            .when()
+            .get(BASE_URL + id)
+            .then()
+            .statusCode(200)
+            .body(matchesJsonSchemaInClasspath("schema/discipline.json"));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "'',    4",
+        "P,     2",
+        "' ',   2",
+        "MT,    1",
+    })
+    void searchReturnContentCountAccordingToQuery(String query, int expectedSize) {
+        given()
+            .queryParam("query", query)
+            .when()
+            .get(BASE_URL + "search")
+            .then()
+            .statusCode(200)
+            .body("content", hasSize(expectedSize))
+            .body("totalElements", is(expectedSize));
+    }
+
+    @Test
+    void searchReturnValidJsonSchema() {
+        given()
+            .queryParam("query", "")
+            .when()
+            .get(BASE_URL + "search")
+            .then()
+            .statusCode(200)
+            .body(matchesJsonSchemaInClasspath("schema/disciplines-with-pagination.json"));
+    }
+}
--- specialization-server/specialization-server-rest/src/integration-test/java/by/kovzov/uis/specialization/rest/controller/DisciplineControllerIT.java
+++ specialization-server/specialization-server-rest/src/integration-test/java/by/kovzov/uis/specialization/rest/controller/DisciplineControllerIT.java
@@ -2,23 +2,15 @@ package by.kovzov.uis.specialization.rest.controller;
 
 import static org.hamcrest.Matchers.blankOrNullString;
 import static org.hamcrest.Matchers.emptyOrNullString;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
-
-import static java.text.MessageFormat.format;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 import by.kovzov.uis.specialization.repository.api.DisciplineRepository;
 import by.kovzov.uis.specialization.repository.entity.Discipline;
@@ -27,91 +19,19 @@ import io.restassured.http.ContentType;
 import lombok.Setter;
 
 @Setter
-class DisciplineControllerIT extends AbstractIntegrationTest {
+public class DisciplineControllerIT extends AbstractIntegrationTest {
 
-    private static final String BASE_URL = "/api/disciplines/";
+    private static final String BASE_URL = "/api/disciplines";
 
     @Autowired
     private DisciplineRepository disciplineRepository;
 
-    private List<Discipline> disciplines;
-
-    @BeforeEach
-    void setUp() {
-        disciplines = dataLoader.loadJson(Discipline.class, "data/json/disciplines.json");
-        disciplineRepository.saveAll(disciplines);
+    @AfterEach
+    void tearDown() {
+        disciplineRepository.deleteAll();
     }
 
     @Test
-    void getByIdReturnValidResponse() {
-        int id = 1;
-
-        given()
-            .when()
-            .get(BASE_URL + id)
-            .then()
-            .statusCode(200)
-            .body("id", is(id))
-            .body("name", is("Spanish"))
-            .body("shortName", is("sp"));
-    }
-
-    @Test
-    void getByIdReturnNotFoundWhenIdNotExist() {
-        int id = 999999;
-
-        given()
-            .when()
-            .get(BASE_URL + id)
-            .then()
-            .statusCode(404)
-            .body("message", is(format("Discipline with id = {0} not found.", id)))
-            .body("path", is(BASE_URL + id));
-    }
-
-    @Test
-    void getByIdReturnValidJsonSchema() {
-        int id = 1;
-
-        given()
-            .when()
-            .get(BASE_URL + id)
-            .then()
-            .statusCode(200)
-            .body(matchesJsonSchemaInClasspath("schema/discipline.json"));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "'',    4",
-        "P,     2",
-        "' ',   2",
-        "MT,    1",
-    })
-    void searchReturnContentCountAccordingToQuery(String query, int expectedSize) {
-        given()
-            .queryParam("query", query)
-            .when()
-            .get(BASE_URL + "search")
-            .then()
-            .statusCode(200)
-            .body("content", hasSize(expectedSize))
-            .body("totalElements", is(expectedSize));
-    }
-
-    @Test
-    void searchReturnValidJsonSchema() {
-        given()
-            .queryParam("query", "")
-            .when()
-            .get(BASE_URL + "search")
-            .then()
-            .statusCode(200)
-            .body(matchesJsonSchemaInClasspath("schema/disciplines-with-pagination.json"));
-    }
-
-    @Test
-    @Transactional
     void createShouldReturnValidJson() {
         given()
             .contentType(ContentType.JSON)
@@ -125,7 +45,6 @@ class DisciplineControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
-    @Transactional
     void createShouldReturnErrorIfDisciplineAlreadyExist() {
         Discipline discipline = buildDiscipline(null);
         disciplineRepository.save(discipline);
@@ -141,17 +60,18 @@ class DisciplineControllerIT extends AbstractIntegrationTest {
     }
 
     @Test
-    @Transactional
     void createShouldReturnErrorIfJsonSchemaIsNotValid() {
         given()
-            .contentType(ContentType.JSON)
-            .body("""
-                    "name": ""
+            .contentType(ContentType.JSON).body(""" 
+                {
+                    "name": "",
+                    "shortName": ""
+                }
                 """)
             .when()
             .post(BASE_URL)
             .then()
-            .statusCode(404);
+            .statusCode(400);
     }
 
     private Discipline buildDiscipline(Long id) {
