b79ca2d26ac9ea55ff14a5d9617803c54cdb1c91
Uladzislau <fenix23707@gmail.com>
2023-02-28 20:34:03 +0300

    Implement update endpoint

--- specialization-server/specialization-server-rest/src/main/java/by/kovzov/uis/specialization/rest/controller/DisciplineController.java
+++ specialization-server/specialization-server-rest/src/main/java/by/kovzov/uis/specialization/rest/controller/DisciplineController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,5 +42,11 @@ public class DisciplineController {
     @ResponseStatus(HttpStatus.CREATED)
     public DisciplineDto create(@RequestBody @Valid DisciplineDto disciplineDto) {
         return disciplineService.create(disciplineDto);
+    }
+
+    @PutMapping("/{id}")
+    public DisciplineDto update(@PathVariable Long id,
+                                @RequestBody @Valid DisciplineDto disciplineDto) {
+        return disciplineService.update(id, disciplineDto);
     }
 }
