8e7e1a52689561e5dc349c8898930256b75604c4
Uladzislau <fenix23707@gmail.com>
2023-03-05 18:20:29 +0300

    Remove sign up endpoint

--- security-server/security-server-service/src/main/java/by/kovzov/uis/security/service/api/AuthService.java
+++
@@ -1,9 +0,0 @@
-package by.kovzov.uis.security.service.api;
-
-import by.kovzov.uis.security.domain.dto.SignupDto;
-import by.kovzov.uis.security.repository.entity.User;
-
-public interface AuthService {
-
-    User signup(SignupDto signupDto);
-}
