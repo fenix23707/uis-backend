f05ed136513144d1871b10904fea754b59cc7bf5
Uladzislau <fenix23707@gmail.com>
2023-03-05 18:18:44 +0300

    Remove rsa key files

--- security-server/security-server-rest/src/main/java/by/kovzov/uis/security/rest/SecurityServerApplication.java
+++ security-server/security-server-rest/src/main/java/by/kovzov/uis/security/rest/SecurityServerApplication.java
@@ -2,16 +2,13 @@ package by.kovzov.uis.security.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import by.kovzov.uis.security.rest.security.RsaKeyProperties;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 @SpringBootApplication(scanBasePackages = {
     "by.kovzov.uis.security",
     "by.kovzov.uis.common.exception.handler"
 })
-@EnableConfigurationProperties(RsaKeyProperties.class)
 @OpenAPIDefinition
 public class SecurityServerApplication {
 
