6ee5892cd2a245aced712a6592d026692aedfd79
Uladzislau Kauzou <fenix23707@gmail.com>
2023-05-27 10:20:29 +0300

    Fix validation

--- academic-server/academic-server-dto/src/main/java/by/kovzov/uis/academic/dto/CurriculumDto.java
+++ academic-server/academic-server-dto/src/main/java/by/kovzov/uis/academic/dto/CurriculumDto.java
@@ -3,6 +3,7 @@ package by.kovzov.uis.academic.dto;
 import java.time.LocalDate;
 
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Value;
@@ -16,6 +17,7 @@ public class CurriculumDto {
 
     @NotNull LocalDate approvalDate;
 
+    @Min(value = 1700, message = "Admission year can not be less than 1700")
     @NotNull Integer admissionYear;
 
     @NotNull Long specializationId;
--- academic-server/academic-server-dto/src/main/java/by/kovzov/uis/academic/dto/SpecializationDto.java
+++ academic-server/academic-server-dto/src/main/java/by/kovzov/uis/academic/dto/SpecializationDto.java
@@ -1,5 +1,6 @@
 package by.kovzov.uis.academic.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Value;
 
@@ -7,8 +8,14 @@ import lombok.Value;
 @Builder(toBuilder = true)
 public class SpecializationDto {
     Long id;
+
+    @NotBlank(message = "Name can not be blank")
     String name;
+
+    @NotBlank(message = "Short name can not be blank")
     String shortName;
+
+    @NotBlank(message = "Cipher can not be blank")
     String cipher;
     Long parentId;
     boolean hasChildren;
